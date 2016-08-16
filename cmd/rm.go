0caa354bbc638d4e82447e5c3b8927930adba6a3
Kunal Kushwaha <kushwaha_kunal_v7@lab.ntt.co.jp>
2016-08-16 16:26:36 +0900

    rmi and rm function complete

--- cmd/rm.go
+++ cmd/rm.go
@@ -1,18 +1,23 @@
 package cmd
 
 import (
+	"fmt"
 	"log"
 
+	"golang.org/x/net/context"
+
 	"github.com/docker/engine-api/client"
+	"github.com/docker/engine-api/types"
+	"github.com/docker/engine-api/types/filters"
 	"github.com/spf13/cobra"
 )
 
 // rmCmd represents the rm command
 var rmCmd = &cobra.Command{
 	Use:   "rm",
-	Short: "deletes docker containers",
-	Long:  `deletes docker containers`,
-	Run:   removeImages,
+	Short: "deletes docker containers in bulk",
+	Long:  `deletes docker containers in bulk`,
+	Run:   removeContainers,
 }
 
 func init() {
@@ -22,12 +27,39 @@ func init() {
 	rmCmd.Flags().BoolP("exited", "e", true, "exited containers")
 }
 
-func removeImages(cmd *cobra.Command, args []string) {
+func removeContainers(cmd *cobra.Command, args []string) {
 	cli, err := client.NewEnvClient()
 	if err != nil {
 		log.Fatal("Unable to communicate with docker daemon")
 	}
+	ctx := context.Background()
 
-	log.Print("API Version : ", cli.ClientVersion())
+	filters := filters.NewArgs()
+	exited, _ := cmd.Flags().GetBool("exited")
+	if exited {
+		filters.Add("status", "exited")
+	}
+
+	options := types.ContainerListOptions{Filter: filters}
+	clist, err := cli.ContainerList(ctx, options)
+	if err != nil {
+		log.Fatal("Error : ", err)
+	}
+
+	removeOptions := types.ContainerRemoveOptions{RemoveVolumes: true, Force: true}
+	dryrun, _ := cmd.Flags().GetBool("dryrun")
+
+	for _, container := range clist {
+		fmt.Print("Removing ", container.ID[0:8], " ", container.Names[0], " ... ")
+		if !dryrun {
+			err := cli.ContainerRemove(ctx, container.ID, removeOptions)
+			if err != nil {
+				fmt.Print(err)
+			} else {
+				fmt.Print("\tRemoved")
+			}
+		}
+		fmt.Println("")
+	}
 
 }
--- cmd/rmi.go
+++ cmd/rmi.go
@@ -2,7 +2,13 @@ package cmd
 
 import (
 	"fmt"
+	"log"
 
+	"golang.org/x/net/context"
+
+	"github.com/docker/engine-api/client"
+	"github.com/docker/engine-api/types"
+	"github.com/docker/engine-api/types/filters"
 	"github.com/spf13/cobra"
 )
 
@@ -11,10 +17,7 @@ var rmiCmd = &cobra.Command{
 	Use:   "rmi",
 	Short: "deletes the docker images",
 	Long:  `deletes the docker images`,
-	Run: func(cmd *cobra.Command, args []string) {
-		// TODO: Work your own magic here
-		fmt.Println("rmi called")
-	},
+	Run:   removeImages,
 }
 
 func init() {
@@ -22,4 +25,41 @@ func init() {
 
 	// Here you will define your flags and configuration settings.
 	rmiCmd.Flags().BoolP("untagged", "u", true, "untagged images")
+}
+
+func removeImages(cmd *cobra.Command, args []string) {
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		log.Fatal("Unable to communicate with docker daemon")
+	}
+	ctx := context.Background()
+
+	filters := filters.NewArgs()
+	exited, _ := cmd.Flags().GetBool("untagged")
+	if exited {
+		filters.Add("dangling", "true")
+	}
+
+	options := types.ImageListOptions{Filters: filters}
+	imageList, err := cli.ImageList(ctx, options)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dryrun, _ := cmd.Flags().GetBool("dryrun")
+	for _, image := range imageList {
+		fmt.Print("Removing ", image.ID[7:16], " ... ")
+
+		if !dryrun {
+			_, err := cli.ImageRemove(ctx, image.ID, types.ImageRemoveOptions{})
+			if err != nil {
+				fmt.Print(err)
+			} else {
+				fmt.Print("\tRemoved")
+			}
+		}
+		fmt.Println("")
+
+	}
+
 }
